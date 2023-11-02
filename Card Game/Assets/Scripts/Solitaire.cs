2ace0f66bcf2528bb740c4658260a4724a6ef553
chuab2 <148381130+chuab2@users.noreply.github.com>
2023-11-02 16:00:06 -0700

    Update Solitaire.cs
    Added a draw function.
    Modified SolitaireSort to use own code instead of video code.
    Added comments to most things for descriptions.

--- Card Game/Assets/Scripts/Solitaire.cs
+++ Card Game/Assets/Scripts/Solitaire.cs
@@ -9,6 +9,8 @@ public class Solitaire : MonoBehaviour
     public GameObject cardPrefab;
     public GameObject[] bottomPos;
     public GameObject[] topPos;
+    public GameObject drawPilePos;
+    public GameObject drawnPos;
 
     public static string[] suits = new string[] {"C", "D", "H", "S"};
     public static string[] values = new string[] {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -33,7 +35,7 @@ public class Solitaire : MonoBehaviour
         PlayCards();
     }
 
-    public void PlayCards()
+    public void PlayCards() // initializes the game by shuffling, sorting, and dealing the starting cards
     {
         deck = GenerateDeck();
         Shuffle(deck);
@@ -47,7 +49,7 @@ public class Solitaire : MonoBehaviour
         SolitaireDeal();
     }
 
-    public static List<string> GenerateDeck()
+    public static List<string> GenerateDeck() // creates the deck based off of the template deck
     {
         List<string> newDeck = new List<string>();
         foreach(string s in suits)
@@ -60,7 +62,7 @@ public class Solitaire : MonoBehaviour
         return newDeck;
     }
 
-    void Shuffle<T>(List<T> list)
+    void Shuffle<T>(List<T> list) // shuffles the starting deck
     {
         randRange = Random.Range(0, 51);
         int n = list.Count;
@@ -74,38 +76,51 @@ public class Solitaire : MonoBehaviour
         }
     }
 
-    void SolitaireDeal()
+    void SolitaireDeal() // physically moves the cards into their starting piles, and flips the top card in each pile
     {
         for (int i = 0; i < 7; i++)
         {
-
             float yOffset = 0;
             float zOffset = 0.03f;
-            foreach (string card in bottoms[i])
+            foreach (string card in bottoms[i]) // for every card in each pile
             {
                 GameObject newCard = Instantiate(cardPrefab, new Vector3(bottmPos[i].transform.position.x, bottomPos[i].transform.position.y - yOffset, bottomPos[i].transform.position.z - zOffset), Quaternion.identity, bottomPos[i].transform);
-                newCard.name = card;
-                if (card == bottoms[i][bottoms[i].Count - 1])
+                newCard.name = card; //names the card so it can be referenced later
+                if (card == bottoms[i].get(bottoms[i].Count - 1))
                 {
                     newCard.GetComponent<Selectable>().faceUp = true;
                 }
 
-                yOffset = yOffset + 0.1f;
+                yOffset = yOffset + 0.1f; //offsets each card so they aren't all stacked on top of each other
                 zOffset = zOffset + 0.03f;
             }
         }
     }
 
-    void SolitaireSort()
+    void SolitaireSort() //sorts all the starting cards into their individual piles
     {
-        for (int i = 0; i < 7; i++)
+        int cardCount = 0;
+        //assign all cards to the bottoms of each pile
+        for (int i = 1; i < 8; i++) //for each pile
         {
-            for (int j = i; j < 7; j++)
+            for(int j = 0; j < i; j++) //add that many cards to the pile and remove it from the starting deck
             {
-                bottoms[j].Add(deck.Last<string>());
-                deck.RemoveAt(deck.Count - 1);
+                bottoms.get(i-1).add(deck.get(cardCount));
+                deck.RemoveAt(cardCount);
+                cardCount++;
             }
         }
     }
 
+    void DrawNextCard() // draws the next card from the draw pile
+    {
+        GameObject drawnCard = GameObject.Find(deck.get(0)); // find the revealed card gameobject
+        drawnCard.transform = new Vector3(drawPilePos.transform.position.x, drawPilePos.transform.position.y, drawPilePos.transform.position.z); // move the current revealed card
+        drawnCard.getComponent<Selectable>().faceUp = false; // flip it back over
+        deck.Insert(deck.Count-1, deck.get(0)); // add a copy of the revealed card to the back
+        deck.RemoveAt(0); // remove the revealed card
+        drawnCard = GameObject.Find(deck.get(0)); // set the revealed card to the next card
+        drawnCard.transform = new Vector3(drawnPos.transform.position.x, drawnPos.transform.position.y, drawnPos.transform.position.z); // move the new first card to the right pos
+        drawnCard.getComponent<Selectable>().faceUp = true; // flip the new first card
+    }
 }
